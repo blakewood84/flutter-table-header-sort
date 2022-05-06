9253f0c8ed30144c651075c9cb311095839468c2
Blake Wood Jr <blake@rewired.one>
2022-05-06 08:57:06 -0700

    created sorting header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -22,13 +25,28 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String? sortBy;
+  Future<dynamic> fetchData() async {
+    try {
+      Uri uri = Uri.parse('http://127.0.0.1:5500/json/table_data.json');
+      final response = await http.get(uri);
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw ErrorDescription('Bad Request!');
+      }
+    } catch (error) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,21 +66,74 @@ class _MyHomePageState extends State<MyHomePage> {
               width: double.infinity,
               decoration: const BoxDecoration(
                 border: Border(
-                  bottom: BorderSide(
-                    width: 1,
-                    color: Colors.black
-                  )
-                )
+                  bottom: BorderSide(width: 1, color: Colors.black),
+                ),
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text('Name'),
-                  Text('Date'),
-                  Text('Type'),
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        sortBy = 'name';
+                      });
+                    },
+                    child: const Text('Name'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        sortBy = 'date';
+                      });
+                    },
+                    child: const Text('Date'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        sortBy = 'type';
+                      });
+                    },
+                    child: const Text('Type'),
+                  ),
                 ],
-              )
+              ),
             ),
+            FutureBuilder(
+              future: fetchData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final data = snapshot.data as List<dynamic>;
+                  if(sortBy != null) {
+                    data.sort(((a, b) => a[sortBy].compareTo(b[sortBy])));
+                  }
+                  return Expanded(
+                    child: ListView.builder(
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        final row = Map<String, String>.from(data[index]);
+                        return Container(
+                          padding: const EdgeInsets.all(20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(row['name']!),
+                              Text(row['date']!),
+                              Text(row['type']!)
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )
           ],
         ),
       ),
